049b5be5307d8e8f9db4b4c5e4cb2415fb7e796e
steven victor <chikodi543@gmail.com>
2019-08-09 09:17:49 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+type Book struct {
+	Title  string
+	Author string
+}
+
+// Declare a Book type which satisfies the fmt.Stringer interface.
+func (b Book) String() string {
+	return fmt.Sprintf("Book: %s - %s", b.Title, b.Author)
+}
+
+// Declare a Count type which satisfies the fmt.Stringer interface.
+type Count int
+
+func (c Count) String() string {
+	return strconv.Itoa(int(c))
+}
+
+// Describe a WriteLog() function which takes any object that satisfies the fmt.Stringer interface as a parameter
+
+func WriteLog(s fmt.Stringer) {
+	log.Println(s.String())
+}
+
+func main() {
+	// Initialize a Book object and pass it to WriteLog()
+	book := Book{"Think and Grow Rich", "Napolean Hill"}
+	WriteLog(book)
+
+	// Initialize a Count object and pass it to WriteLog()
+	count := Count(3)
+	WriteLog(count)
+
+}
